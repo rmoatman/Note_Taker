227bbd6a5b7b7c652b7fb79329c07851b06f0da8
RMOATMAN <raemarie.oatman@gmail.com>
2021-07-18 20:52:33 -0400

    edited and added to api routes

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,3 +1,18 @@
+// FUNCTION LIST
+// show -- Shows an element
+// hide -- Hides an element
+// getNotes -- Retrieves saved notes using GET api/notes
+// saveNote -- Saves a new note using POST api/note
+// deletesNote -- Deletes an existing note using DELETE api/notes
+// renderActiveNote -- Hides save button when user clicks to enter new note also assigns value to noteTitle and noteText 
+// handleNoteSave -- Creates new note object then calls getAndRenderNotes and renderActiveNote
+// handleNoteDelete -- Deletes the clicked note
+// handleNoteView -- Sets the activeNote and displays it
+// handleNewNoteView -- Sets the activeNote to and empty object and allows the user to enter a new note
+// handleRenderSaveBtn -- Hides save button if value exists in new note title and text
+// renderNoteList -- Renders the list of note titles
+
+// VARIABLES
 let noteTitle;
 let noteText;
 let saveNoteBtn;
@@ -12,27 +27,30 @@ if (window.location.pathname === "/notes") {
   noteList = document.querySelectorAll(".list-container .list-group");
 }
 
-// Show an element
+
+// Shows an element
 const show = (elem) => {
   elem.style.display = "inline";
-};
+}; // end show
 
-// Hide an element
+// Hides an element
 const hide = (elem) => {
   elem.style.display = "none";
-};
+}; // end hide
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
+// Retrieves saved notes using GET api/notes
 const getNotes = () =>
   fetch("/api/notes", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
-  });
+  }); // end getNotes
 
+// Saves a new note using POST api/note
 const saveNote = (note) =>
   fetch("/api/notes", {
     method: "POST",
@@ -40,16 +58,22 @@ const saveNote = (note) =>
       "Content-Type": "application/json",
     },
     body: JSON.stringify(note),
-  });
+    // note refers to the variable representing a new note
+  }); // end saveNote
 
+// Deletes an existing note using DELETE api/notes
+// id is passed in from line 20
 const deleteNote = (id) =>
+// see line 83 in express.js
   fetch(`/api/notes/${id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
-  });
+  }); // end deleteNote
 
+// Hides save button when user clicks into note title to enter data
+// also assigns value to noteTitle and noteText
 const renderActiveNote = () => {
   hide(saveNoteBtn);
 
@@ -64,8 +88,9 @@ const renderActiveNote = () => {
     noteTitle.value = "";
     noteText.value = "";
   }
-};
+}; // end renderActiveNote
 
+// Creates new note object then calls getAndRenderNotes and renderActiveNote
 const handleNoteSave = () => {
   const newNote = {
     title: noteTitle.value,
@@ -75,49 +100,54 @@ const handleNoteSave = () => {
     getAndRenderNotes();
     renderActiveNote();
   });
-};
+}; // end handleNoteSave
 
-// Delete the clicked note
+// Deletes the clicked note
 const handleNoteDelete = (e) => {
   // Prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
 
+  // e is the click event
   const note = e.target;
+  // gets the id of the clicked note
   const noteId = JSON.parse(note.parentElement.getAttribute("data-note")).id;
 
   if (activeNote.id === noteId) {
     activeNote = {};
   }
 
+  // passes the note id to deletNoteFunction line 66
   deleteNote(noteId).then(() => {
     getAndRenderNotes();
     renderActiveNote();
   });
-};
+}; // end handleNoteDelete
 
 // Sets the activeNote and displays it
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute("data-note"));
   renderActiveNote();
-};
+}; // end handleNoteView
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
-};
+}; // end handleNewNoteView
 
+// Hides save button if value exists in new note title and text
 const handleRenderSaveBtn = () => {
   if (!noteTitle.value.trim() || !noteText.value.trim()) {
     hide(saveNoteBtn);
   } else {
     show(saveNoteBtn);
   }
-};
+}; // end handleRenderSaveBtn
 
-// Render the list of note titles
+// Renders the list of note titles
 const renderNoteList = async (notes) => {
+  // notes is the object array containing the notes
   let jsonNotes = await notes.json();
   if (window.location.pathname === "/notes") {
     noteList.forEach((el) => (el.innerHTML = ""));
@@ -154,7 +184,7 @@ const renderNoteList = async (notes) => {
     return liEl;
   };
 
-  if (jsonNotes.length === 0) {
+    if (jsonNotes.length === 0) {
     noteListItems.push(createLi("No saved Notes", false));
   }
 
@@ -168,7 +198,7 @@ const renderNoteList = async (notes) => {
   if (window.location.pathname === "/notes") {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
-};
+}; // end renderNoteList
 
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
@@ -180,4 +210,5 @@ if (window.location.pathname === "/notes") {
   noteText.addEventListener("keyup", handleRenderSaveBtn);
 }
 
+console.log("begin");
 getAndRenderNotes();
--- express.js
+++ express.js
@@ -1,15 +1,97 @@
 // DEPENDENCIES
 const express = require("express");
+const fs = require("fs");
+const { uid } = require("uid");
+
+// VARIABLES
+const data = fs.readFileSync("./Develop/db/db.json", "utf-8");
+const noteList = JSON.parse(data);
+
 
 // EXPRESS CONFIGURATION
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require("path");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ROUTER
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// FUNCTIONS
+// save note to list
+function saveNotes(notes) {
+  fs.writeFile(
+    path.join(__dirname, "/Develop/db/db.json"),
+    JSON.stringify(notes),
+    (err) => (err ? console.err(err) : console.log("Note File Saved!"))
+  );
+}
+
+// ROUTES
+
+// HTML Routes
+// root
+app.get("/", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+// notes
+app.get("/notes", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+
+
+
+// Other Routes
+// javascript
+app.get("/assets/js/index.js", (req, res) =>
+  res.sendFile(path.join(__dirname, "/Develop/public/assets/js/index.js"))
+);
+
+// css
+app.get("/assets/css/styles.css", (req, res) => {
+  res.set("Content-Type", "text/css");
+  res.sendFile(path.join(__dirname, "/Develop/public/assets/css/styles.css"));
+});
+
+// API Routes
+// get notes
+app.get("/api/notes", (req, res) => {
+  res.set("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname, "Develop/db/db.json"));
+});
+
+// if route not found
+app.get("*", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+// create note ID
+app.post("/api/notes", (req, res) => {
+  let note = req.body;
+  let newID = uid(4);
+  note.id = newID;
+  noteList.push(note);
+
+  saveNotes(noteList);
+  res.send(note);
+});
+
+// delete note
+// uid is passed in from index.js line 68
+app.delete("/api/notes/:uid", (req, res) => {
+  for (let i = 0; i < noteList.length; i++) {
+    if (noteList[i].id === req.params.uid) {
+      noteList.splice(i, 1);
+      console.log("Note Successfully Deleted!")
+    }
+  }
+ saveNotes(noteList);
+  res.end();
+});
+
 
 // LISTENER
 app.listen(PORT, () => {
